bb5f64ab1e066639deb3670c2d33e9a650ffe0ee
wei cui <cwhe@qq.com>
2016-01-12 16:40:22 +0800

    4.1.3.4新特性

--- idleApp/UpdateForm.Designer.cs
+++ idleApp/UpdateForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UpdateForm));
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.label1 = new System.Windows.Forms.Label();
             this.progressLabel = new System.Windows.Forms.Label();
@@ -66,6 +67,7 @@
             this.Controls.Add(this.progressLabel);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.progressBar1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "UpdateForm";
